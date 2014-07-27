c2ee4f9c1584e0c14f48bc54f009186c666b1c47
perfectHobo <lad5348573@mail.ru>
2014-07-27 16:32:54 +0300

    ...

--- second week/1.py
+++ second week/1.py
@@ -1,11 +1,17 @@
+
+# Напишите функцию input_int(a, b), которая будет работать.
+# так же как функция input() без параметров, но только она
+# будет проверять, что введённая пользователем строка
+# действительно является числом в интервале [a;b],
+# и если это не так, то будет просить пользователя повторить попытку ввода
 def input_int(minnum, maxnum):
     userinp = input()
     while True:
-        if userinp.isdigit() and minnum < int(userinp) < maxnum:  
-                print ("неошибка")
-                break
+        if userinp.isdigit() and minnum < int(userinp) < maxnum:
+            input()
+            break
         else:
-            print ("ошибка")
+            print ("повторите попытку")
             break
     return userinp
 minnum = 1
